fb791a4a849564abd70e2c78aace1f9f79010e2c
Rajendra Acharya <acharyarajendra48@gmail.com>
2026-04-30 18:55:37 +0530

    after adding more options

--- README.md
+++ README.md
@@ -1,50 +1,80 @@
-Pokedex Lite
-
-About the Project
+# Pokedex Lite 
+## About the Project
 
 Pokedex Lite is a simple React web app where you can explore Pokémon data. It uses the PokéAPI to fetch information and shows it in a clean and easy-to-use interface.
 
 I built this project mainly to practice working with APIs, React state management, and responsive design using Tailwind CSS. I also added some animations using Framer Motion to make the UI feel smoother.
 
-Features
-Search Pokémon by name
-Filter Pokémon by type
-Pagination to move through the list
-Add/remove favorites (stored in local storage)
-View detailed Pokémon info like stats, abilities, height, and weight
-Fully responsive for mobile, tablet, and desktop
-Smooth animations for cards and popup modal
+---
 
-Tech Stack
-React (Vite)
-Tailwind CSS
-Framer Motion
-PokéAPI
 
-How to Run
+## Features
+
+*Search Pokémon by name
+*Filter Pokémon by type
+*Pagination to move through the list
+*Add/remove favorites (stored in local storage)
+*View detailed Pokémon info like stats, abilities, height, and weight
+*Fully responsive for mobile, tablet, and desktop
+*Smooth animations for cards and popup modal
+
+---
+
+
+## Tech Stack
+
+*React (Vite)
+*Tailwind CSS
+*Framer Motion
+*PokéAPI
+
+---
+
+
+## How to Run
+bash
 npm install
 npm run dev
 
-Live Demo
+---
+
+
+## Live Demo
 
 https://pokedex--lite.vercel.app
 
-Challenges I Faced
+
+---
+
+
+
+## Challenges I Faced
 
 The main challenge was handling multiple API calls when loading Pokémon details. I fixed this using Promise.all() to make it faster.
 
 Another challenge was managing search, filter, pagination, and favorites together without breaking the UI flow.
 
-What I Learned
-How to work with APIs using async/await
-How to manage state in React
-How to build reusable components
-How to make responsive layouts using Tailwind CSS
-How to add animations using Framer Motion
-How to handle multiple UI states properly
-Future Improvements
-Add login system
-Improve performance
-Add infinite scroll instead of pagination
-Add more filters
-Improve UI animations
+---
+
+
+
+## What I Learned
+*How to work with APIs using async/await
+*How to manage state in React
+*How to build reusable components
+*How to make responsive layouts using Tailwind CSS
+*How to add animations using Framer Motion
+*How to handle multiple UI states properly
+
+---
+
+
+## Future Improvements
+
+*Add login system
+*Improve performance
+*Add infinite scroll instead of pagination
+*Add more filters
+*Improve UI animations
+
+---
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { fetchPokemonList, fetchPokemonDetails } from "./services/api";
 import PokemonCard from "./components/PokemanCard";
 import PokemonModal from "./components/PokemonModal";
 import logo from './assets/logos.png'
+import header from './assets/pokedexheader.png'
 
 export default function App() {
   const [search, setSearch] = useState("");
@@ -74,84 +75,101 @@ export default function App() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      className="min-h-screen bg-gradient-to-b from-yellow-200 via-blue-200 to-green-200 p-4"
+      className="min-h-screen bg-gradient-to-b from-yellow-200 via-blue-200 to-green-200 "
     >
-     <div className="flex items-center justify-center gap-3">
-  <img
-    src={logo}
-    alt="logo"
-    className="w-10 h-10 animate-bounce mb-12"
-  />
+      <div className="w-full flex items-center justify-center gap-3  bg-gradient-to-r from-red-400 to-red-700 m-0 p-2 mb-5">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-10 h-10 animate-bounce"
+        />
+        <img
+          src={header}
+          alt="header"
+          className="h-12 object-contain"
+        />
+      </div>
 
-  <h1 className="text-4xl font-extrabold text-red-600 mb-12">
-    Pokedex Lite
-  </h1>
-</div>
+      <div className="p-4">
+        <input
+          type="text"
+          placeholder="Search Pokemon..."
+          className="w-full p-3 border-4 border-yellow-400 rounded-full mb-4"
+          onChange={(e) => setSearch(e.target.value)}
+        />
 
-      <input
-        type="text"
-        placeholder="Search Pokemon..."
-        className="w-full p-3 border-4 border-yellow-400 rounded-full mb-4"
-        onChange={(e) => setSearch(e.target.value)}
-      />
+        <select
+          onChange={(e) => setType(e.target.value)}
+          className="w-full p-3 border-4 border-yellow-400 rounded-full mb-4"
+        >
+          <option value="">All Types</option>
+          <option value="fire">Fire</option>
+          <option value="water">Water</option>
+          <option value="grass">Grass</option>
+          <option value="electric">Electric</option>
+          <option value="ice">Ice</option>
+          <option value="fighting">Fighting</option>
+          <option value="poison">Poison</option>
+          <option value="ground">Ground</option>
+          <option value="flying">Flying</option>
+          <option value="psychic">Psychic</option>
+          <option value="bug">Bug</option>
+          <option value="rock">Rock</option>
+          <option value="ghost">Ghost</option>
+          <option value="dragon">Dragon</option>
+          <option value="dark">Dark</option>
+          <option value="steel">Steel</option>
+          <option value="fairy">Fairy</option>
+          <option value="normal">Normal</option>
+        </select>
 
-      <select
-        onChange={(e) => setType(e.target.value)}
-        className="w-full p-3 border-4 border-yellow-400 rounded-full mb-4"
-      >
-        <option value="">All Types</option>
-        <option value="fire">🔥 Fire</option>
-        <option value="water">💧 Water</option>
-        <option value="grass">🌱 Grass</option>
-        <option value="electric">⚡ Electric</option>
-      </select>
-
-      {error ? (
-        <div className="text-red-600 font-bold text-center mt-10">
-          {error}
-        </div>
-      ) : loading ? (
-        <div className="flex flex-col items-center justify-center mt-20">
-          <div className="w-12 h-12 border-4 border-yellow-400 border-t-transparent rounded-full animate-spin"></div>
-          <p className="mt-4 text-gray-700 font-semibold">Loading Pokémon...</p>
-        </div>
-      ) : (
-        <motion.div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-          {filteredPokemon.map((p) => (
-            <PokemonCard
-              key={p.id}
-              pokemon={p}
-              isFavorite={favorites.includes(p.name)}
-              toggleFavorite={toggleFavorite}
-              onClick={() => setSelectedPokemon(p)}
-            />
-          ))}
-        </motion.div>
-      )}
-
-      <AnimatePresence>
-        {selectedPokemon && (
-          <PokemonModal
-            pokemon={selectedPokemon}
-            onClose={() => setSelectedPokemon(null)}
-          />
+        {error ? (
+          <div className="text-red-600 font-bold text-center mt-10">
+            {error}
+          </div>
+        ) : loading ? (
+          <div className="flex flex-col items-center justify-center mt-20">
+            <div className="w-12 h-12 border-4 border-yellow-400 border-t-transparent rounded-full animate-spin "></div>
+            <p className="mt-4 text-gray-700 font-semibold">Loading Pokémon...</p>
+          </div>
+        ) : (
+          <motion.div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+            {filteredPokemon.map((p) => (
+              <PokemonCard
+                key={p.id}
+                pokemon={p}
+                isFavorite={favorites.includes(p.name)}
+                toggleFavorite={toggleFavorite}
+                onClick={() => setSelectedPokemon(p)}
+              />
+            ))}
+          </motion.div>
         )}
-      </AnimatePresence>
 
-      <div className="flex flex-row sm:flex-row items-center justify-center gap-4 sm:gap-10 mt-4">
+        <AnimatePresence>
+          {selectedPokemon && (
+            <PokemonModal
+              pokemon={selectedPokemon}
+              onClose={() => setSelectedPokemon(null)}
+            />
+          )}
+        </AnimatePresence>
 
-        <button className="px-4 py-2 bg-yellow-400 rounded-full flex items-center gap-2" onClick={() => setPage((p) => Math.max(p - 1, 0))}>
-          ← Prev
-        </button>
+        <div className="flex flex-row sm:flex-row items-center justify-center gap-4 sm:gap-10 mt-4">
 
-        <span className="text-lg font-semibold">
-          Page {page}
-        </span>
+          <button className="px-4 py-2 bg-yellow-400 rounded-full flex items-center gap-2" onClick={() => setPage((p) => Math.max(p - 1, 0))}>
+            ← Prev
+          </button>
 
-        <button className="px-4 py-2 bg-yellow-400 rounded-full flex items-center gap-2" onClick={() => setPage((p) => p + 1)}>
-          Next →
-        </button>
+          <span className="text-lg font-semibold">
+            Page {page}
+          </span>
 
+          <button className="px-4 py-2 bg-yellow-400 rounded-full flex items-center gap-2" onClick={() => setPage((p) => p + 1)}>
+            Next →
+          </button>
+
+        </div>
       </div>
     </motion.div>
   );
